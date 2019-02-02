7ef79a840e02328169e6b0fa2f58531ea474b89d
1-nty <46821853+1-nty@users.noreply.github.com>
2019-02-02 15:05:09 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,48 @@
+
+/*****************prototype-based class ***********/
+function User(name){
+  this.name = name;
+}
+
+User.prototype.sayHi = function(){
+  console.log(this.name);
+}
+
+let user = new User("John");
+user.sayHi();
+
+
+/*****************class syntax ********************/
+class User {
+  
+  constructor(name){
+    this.name = name;
+  }
+  
+  sayHi(){
+    console.log(this.name);
+  }
+  
+}
+
+let user = new User("John");
+user.sayHi();
+
+User.prototype.test = 5;
+
+
+/*****************object literal ******************/
+var myObject = {
+    sProp: 'some string value',
+    numProp: 2,
+    bProp: false
+    images: ["smile.gif", "grim.gif", "frown.gif", "bomb.gif"],
+    pos: { // nested object literal
+        x: 40,
+        y: 300
+    },
+    onSwap: function() { // function
+        // code here
+    }
+  
+};
